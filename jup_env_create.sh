7468104b81af6f311d05b6f96bba0c1f5e07f6cf
root <root@UserPC.localdomain>
2022-09-26 19:48:29 +0400

    upload scripts

---
+++ jup_env_create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+service docker stop
+
+sleep 1
+while true; do
+    st=`service docker status | grep "is running"`
+    if [ -n "$st" ]; then
+        echo "docker is running"
+        break;
+    else service docker start; sleep 3; echo "docker is not running"
+    fi
+done
+
+docker run -i -d -p 8888:8888 \
+--hostname practicum \
+--mount type=bind,source=/opt/notebooks,target=/opt/notebooks \
+--name practicum continuumio/miniconda3 /bin/bash
+
+
+docker exec practicum wget "https://code.s3.yandex.net/data-analyst/ds_practicum_env.yml"
+docker exec practicum /opt/conda/bin/conda env create -f ./ds_practicum_env.yml
+docker exec practicum /opt/conda/bin/conda activate prakticum
+docker exec practicum /opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks
+docker exec practicum /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
+
+docker stop practicum
+
+exit;
---
+++ start_jupyter.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+service docker stop
+
+sleep 1
+while true; do
+    st=`service docker status | grep "is running"`
+    if [ -n "$st" ]; then
+        echo "docker is running"
+        break;
+    else service docker start; sleep 3; echo "docker is not running"
+    fi
+done
+
+docker start practicum
+
+sleep 3
+while true; do
+    miniconda3=`docker ps | grep 'practicum' | awk '{ print $1 }'`
+    if [ -n "$miniconda3" ]; then
+        echo "miniconda3 is running"
+        break;
+    else sleep 3; echo "miniconda3 is not running"
+    fi
+done
+
+nohup docker exec $miniconda3 conda run -n practicum --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > jupyter.log &
+
+sleep 5
+while true; do
+    lsize=`wc -c /root/jupyter.log | awk '{ print $1 }'`
+    if [ $lsize > 1000 ]; then
+        cat /root/jupyter.log
+        echo 'other ip:'
+        ip route get 8.8.8.8 |awk '{ print $7 }'
+        echo '' > /root/jupyter.log
+        break;
+    else sleep 3; echo "jupyter is not running"
+    fi
+done
+
+while true; do
+    echo $jpid
+    read -p "Do you wish to S top miniconda3 or R estart jupyter?  " yn
+    case $yn in
+#        [Ss]* ) docker stop $miniconda3; docker system prune -a -f; break;;
+        [Ss]* ) docker stop $miniconda3; break;;
+        [Rr]* ) pkill -f jupyter; nohup docker exec $miniconda3 conda run -n praktikum_new --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > jupyter.log &;;
+        * ) echo "Please answer S to close or R to restart";;
+    esac
+done
+
+exit;
