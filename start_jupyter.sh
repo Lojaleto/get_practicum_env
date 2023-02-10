bb5006fdd257a2295cbd5090efa47ff1bcf36d68
Lojaleto <yes@loj.me>
2023-02-10 19:57:37 +0400

    up

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -27,29 +27,33 @@ while true; do
     fi
 done
 
-nohup docker exec $miniconda3 conda run -n $env --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > ./jupyter.log &
-
-sleep 5
 while true; do
-    lsize="$(wc -c ./jupyter.log | awk '{ print $1 }')"
-    if [[ $lsize > 1000 ]]; then
-        cat ./jupyter.log
-        echo 'other ip:'
-        ip route get 8.8.8.8 |awk '{ print $7 }'
-        echo '' > ./jupyter.log
-        break;
-    else sleep 3; echo "jupyter is not running"
-    fi
-done
 
-while true; do
-    echo $jpid
-    read -p "Do you wish to S top miniconda3 or R estart jupyter?  " yn
-    case $yn in
-        [Ss]* ) docker stop $miniconda3; break;;
-        [Rr]* ) pkill -f jupyter; nohup docker exec $miniconda3 conda run -n $env --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > ./jupyter.log &;;
-        * ) echo "Please answer S to close or R to restart";;
-    esac
+    nohup docker exec $miniconda3 conda run -n $env --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > ./jupyter.log &
+
+    sleep 5
+    while true; do
+        lsize="$(wc -c ./jupyter.log | awk '{ print $1 }')"
+        if [[ $lsize > 1000 ]]; then
+            cat ./jupyter.log
+            echo 'other ip:'
+            ip route get 8.8.8.8 |awk '{ print $7 }'
+            echo '' > ./jupyter.log
+            break;
+        else sleep 3; echo "jupyter is not running"
+        fi
+    done
+
+    while true; do
+        echo $jpid
+        read -p "Do you wish to S top miniconda3 or R estart jupyter?  " yn
+        case $yn in
+            [Ss]* ) docker stop $miniconda3; exit;;
+            [Rr]* ) pkill -f jupyter;;
+            * ) echo "Please answer S to close or R to restart";;
+        esac
+    done
+
 done
 
 exit;
